68b858da8b9c2e468650ad56763fddd971fab3dd
zh_new <739493331@qq.com>
2020-09-24 00:08:59 +0800

    抽离CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
+    //配置优化规则
     //配置环境  production生产环境    development开发环境
     mode: 'development', //不指定 默认为生产环境 
     entry: __dirname + "/src/main",//已多次提及的唯一入口文件
     output: {  //输出配置
-      //打包后的文件存放的地方
+      //打包后的文件存放的地方 、、这里必须为绝对路径
       //__dirname指在当前webpack.config.js文件路径下 生成一个dist文件夹
       path: path.resolve(__dirname, 'dist'),
       //打包后输出文件的文件名   文件名后加.min打包后为压缩文件   在默认的生产环境中不加.min也是生成压缩后的文件
       //.[hash] 每一次生成的文件名都带着哈希值 也是为了清除缓存
       filename: "bundle.[hash].js"
     },
+    devtool: "inline-source-map",  //这是消除一个警告，待研究？？
     //开发环境才需要的配置
+    //启动一个本地服务， 会将编译好的文件放进虚拟内存当中，可以在控制台 Sources中查看
     devServer: {
       port: 3000, //端口号
       progress: true, //显示打包编译的进度
@@ -36,6 +41,9 @@ module.exports = {
         //   collapseBooleanAttributes: true,
         //   ....
         // }
+      }),
+      new MiniCssExtractPlugin({
+        filename: './css/[name].css',
       })
     ],
     //使用加载器loader处理规则  打包CSS 打包之前需要在全局入口文件 main.js中引入css文件
@@ -44,7 +52,8 @@ module.exports = {
         test: /\.(css|scss)$/, //基于正则匹配处理那些文件
         //使用那些loader  (执行顺序：从右到左执行解析)
         use: [
-          "style-loader", //把编译好的CSS插入到页面(index.html)的头部head标签中的style标签中去
+          MiniCssExtractPlugin.loader, //抽离css
+          // "style-loader", //把编译好的CSS插入到页面(index.html)的头部head标签中的style标签中去
           "css-loader",   //解析css文件中@import/url()
           "postcss-loader",  //使用autoprefixer为CSS样式添加前缀  这里需要安装autoprefixer和postcss-loader
           //注意: autoprefixer 安装的时候要是8.0.0版本  高版本可能会报错(版本不兼容) npm i autoprefixer@8.0.0 --save-dev 安装8.0版本
